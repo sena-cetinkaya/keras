460ae4ef6f87704ee521b98ab717b7a982a7e82c
Sena Çetinkaya <67286519+sena-cetinkaya@users.noreply.github.com>
2023-09-06 19:22:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# import "numpy" and other "keras" libraries.
+from numpy import loadtxt
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+# We downloaded  the "pima-indians-diabetes.csv" dataset.
+data = loadtxt('pima-indians-diabetes.csv', delimiter=',')
+
+# Getting the input (X) and output (y) values.
+X = data[:,0:8]
+y = data[:,8]
+
+# Creating a 3-layer keras model.
+model = Sequential()
+model.add(Dense(12, input_shape=(8,), activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+
+# Compile the keras model.
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+
+# Fit the keras model on the dataset.
+model.fit(X, y, epochs=150, batch_size=10)
+
+# Learning the accuracy of the keras model.
+_, accuracy = model.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy*100))
+
+
